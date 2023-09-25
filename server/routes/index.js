90c7c56ff2889ddf440946c530a47a0fca79d08c
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-26 00:00:15 +0545

    routes.all added

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,4 +2,13 @@ const router = require("express").Router();
 const apiRouter = require("./routes.api");
 
 router.use("/api/v1", apiRouter);
+
+router.all("*", (req, res, next) => {
+  try {
+    res.status(404).json({ data: "", msg: "Routes not found" });
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
