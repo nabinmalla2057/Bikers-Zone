17d4ff9225e61bdc0f07e1c2dd304067185e5749
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-26 23:59:12 +0545

    vgalidation

--- server/modules/users/user.model.js
+++ server/modules/users/user.model.js
@@ -1,10 +1,26 @@
 const { Schema, model } = require("mongoose");
 const commonSchema = require("../../utils/commonSchema");
 
+const validateEmail = function (email) {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+};
+
 const userSchema = new Schema({
-  name: {},
-  email: {},
+  name: { type: "String", require: "full name needed..." },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
   password: {},
+  ...commonSchema,
 });
 
 module.exports = model("User", userSchema);
