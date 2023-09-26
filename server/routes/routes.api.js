8b53f5e8ef8cf3561fdd458bff9c3a5a6d410ce1
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-26 23:34:24 +0545

    api connected to user

--- server/routes/routes.api.js
+++ server/routes/routes.api.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
+const userRouter = require("../modules/users/user.api");
+
 router.get("/", (req, res, next) => {
   res.json({ dat: "", msg: "success" });
+});
+router.use("/users", userRouter);
+router.all("*", (req, res, next) => {
+  try {
+    res.status(404).json({ data: "", msg: "Routes not found" });
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
