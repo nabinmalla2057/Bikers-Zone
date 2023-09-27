1728d179867ba3d2fae7628c5f7cb4d78aeef880
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-27 23:26:01 +0545

    validation done

--- server/modules/users/user.model.js
+++ server/modules/users/user.model.js
@@ -1,9 +1,6 @@
 const { Schema, model } = require("mongoose");
 const commonSchema = require("../../utils/commonSchema");
-
-const validateEmail = function (email) {
-  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-};
+const { validateEmail } = require("./user.validation");
 
 const userSchema = new Schema({
   name: { type: "String", require: "full name needed..." },
@@ -14,12 +11,11 @@ const userSchema = new Schema({
     unique: true,
     required: "Email address is required",
     validate: [validateEmail, "Please fill a valid email address"],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
-    ],
   },
-  password: {},
+  password: {
+    type: String,
+    required: true,
+  },
   ...commonSchema,
 });
 
---
+++ server/modules/users/user.validation.js
@@ -0,0 +1,5 @@
+const validateEmail = function (email) {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+};
+
+module.exports = { validateEmail };
