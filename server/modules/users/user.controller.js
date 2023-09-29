cf5bb2bce481cbce9b172300e9e79a84e1d1fa13
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-29 21:37:22 +0545

    hash password comparison

--- server/modules/users/user.controller.js
+++ server/modules/users/user.controller.js
@@ -10,6 +10,12 @@ const create = async (payload) => {
   return Model.create(rest);
 };
 
-const login = (email, password) => {};
+const login = async (email, password) => {
+  const isValidUser = await Model.findOne({ email });
+  if (!isValidUser) throw new Error("User not found");
+  const isValid = await bcrypt.compare(password, isValidUser.password);
+  if (!isValid) throw new Error("Email or pass is Invalid");
+  return true;
+};
 
 module.exports = { create, login };
